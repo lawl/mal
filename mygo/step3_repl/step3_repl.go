6f9f61a9ea4699929dbbd5227dc94e67537b613e
hxbrn <git@dumbinter.net>
2019-10-01 12:33:18 +0200

    use environment container

--- mygo/step3_repl/step3_repl.go
+++ mygo/step3_repl/step3_repl.go
@@ -15,13 +15,13 @@ func read(s string) (mal.Type, error) {
 	return ast, nil
 }
 
-func eval(ast mal.Type, replEnv map[string]func(args ...mal.Type) mal.Type) (mal.Type, error) {
+func eval(ast mal.Type, env *mal.Env) (mal.Type, error) {
 	switch v := ast.(type) {
 	case *mal.List:
 		if len(v.Value) == 0 {
 			return ast, nil
 		}
-		ev, err := evalAst(v, replEnv)
+		ev, err := evalAst(v, env)
 		if err != nil {
 			return nil, err
 		}
@@ -30,23 +30,22 @@ func eval(ast mal.Type, replEnv map[string]func(args ...mal.Type) mal.Type) (mal
 		return fn.Value(lst.Value[1:]...), nil
 
 	default:
-		return evalAst(v, replEnv)
+		return evalAst(v, env)
 	}
 }
 
-func evalAst(ast mal.Type, replEnv map[string]func(args ...mal.Type) mal.Type) (mal.Type, error) {
+func evalAst(ast mal.Type, env *mal.Env) (mal.Type, error) {
 	switch v := ast.(type) {
 	case *mal.Symbol:
-		//TODO could be a variable too, probably wrong. fix later
-		fn, ok := replEnv[v.Value]
-		if !ok {
+		val := env.Get(v)
+		if val == nil {
 			return nil, fmt.Errorf("Unknown symbol '%s'", v.Value)
 		}
-		return &mal.Function{Value: fn}, nil
+		return val, nil
 	case *mal.List:
 		var list mal.List
 		for _, val := range v.Value {
-			evaled, err := eval(val, replEnv)
+			evaled, err := eval(val, env)
 			if err != nil {
 				return nil, err
 			}
@@ -63,29 +62,27 @@ func print(ast mal.Type) {
 }
 
 func rep(s string) {
-
-	replEnv := map[string]func(args ...mal.Type) mal.Type{
-		"+": func(args ...mal.Type) mal.Type {
-			a, _ := args[0].(*mal.Number)
-			b, _ := args[1].(*mal.Number)
-			return &mal.Number{Value: a.Value + b.Value}
-		},
-		"-": func(args ...mal.Type) mal.Type {
-			a, _ := args[0].(*mal.Number)
-			b, _ := args[1].(*mal.Number)
-			return &mal.Number{Value: a.Value - b.Value}
-		},
-		"*": func(args ...mal.Type) mal.Type {
-			a, _ := args[0].(*mal.Number)
-			b, _ := args[1].(*mal.Number)
-			return &mal.Number{Value: a.Value * b.Value}
-		},
-		"/": func(args ...mal.Type) mal.Type {
-			a, _ := args[0].(*mal.Number)
-			b, _ := args[1].(*mal.Number)
-			return &mal.Number{Value: a.Value / b.Value}
-		},
-	}
+	replEnv := mal.NewEnv(nil)
+	replEnv.Set(&mal.Symbol{Value: "+"}, &mal.Function{Value: func(args ...mal.Type) mal.Type {
+		a, _ := args[0].(*mal.Number)
+		b, _ := args[1].(*mal.Number)
+		return &mal.Number{Value: a.Value + b.Value}
+	}})
+	replEnv.Set(&mal.Symbol{Value: "-"}, &mal.Function{Value: func(args ...mal.Type) mal.Type {
+		a, _ := args[0].(*mal.Number)
+		b, _ := args[1].(*mal.Number)
+		return &mal.Number{Value: a.Value - b.Value}
+	}})
+	replEnv.Set(&mal.Symbol{Value: "*"}, &mal.Function{Value: func(args ...mal.Type) mal.Type {
+		a, _ := args[0].(*mal.Number)
+		b, _ := args[1].(*mal.Number)
+		return &mal.Number{Value: a.Value * b.Value}
+	}})
+	replEnv.Set(&mal.Symbol{Value: "/"}, &mal.Function{Value: func(args ...mal.Type) mal.Type {
+		a, _ := args[0].(*mal.Number)
+		b, _ := args[1].(*mal.Number)
+		return &mal.Number{Value: a.Value / b.Value}
+	}})
 
 	ast, err := read(s)
 	if err != nil {
