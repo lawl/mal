abcb0ef29df5ae78eaec28b954d10d45c70be377
hxbrn <git@dumbinter.net>
2019-10-03 07:33:43 +0200

    one of the rare cases where goto is more readable and clean than a loop

--- mygo/step5_tco/step5_tco.go
+++ mygo/step5_tco/step5_tco.go
@@ -34,108 +34,106 @@ func setBindingInEnv(env *mal.Env, binding []mal.Type) (mal.Type, error) {
 }
 
 func eval(ast mal.Type, env *mal.Env) (mal.Type, error) {
-	for {
-		switch v := ast.(type) {
-		case *mal.List:
-			if len(v.Value) == 0 {
-				return ast, nil
-			}
+tailcalloptimized:
+	switch v := ast.(type) {
+	case *mal.List:
+		if len(v.Value) == 0 {
+			return ast, nil
+		}
 
-			// if the first element of the list is a symbol, check for special handling, such as "def!"
-			if symb, ok := v.Value[0].(*mal.Symbol); ok {
-				switch symb.Value {
-				case "def!":
-					//check argument length
-					if len(v.Value) != 3 {
-						return nil, fmt.Errorf("'def!' expects exactly 2 paramters")
+		// if the first element of the list is a symbol, check for special handling, such as "def!"
+		if symb, ok := v.Value[0].(*mal.Symbol); ok {
+			switch symb.Value {
+			case "def!":
+				//check argument length
+				if len(v.Value) != 3 {
+					return nil, fmt.Errorf("'def!' expects exactly 2 paramters")
+				}
+				return setBindingInEnv(env, v.Value[1:])
+			case "let*":
+				newEnv := mal.NewEnv(env, nil, nil)
+				if len(v.Value) < 3 {
+					return nil, fmt.Errorf("'let*' expects at least 2 paramters")
+				}
+				if bindings, ok := v.Value[1].(*mal.List); ok {
+					for i := 0; i < len(bindings.Value)/2; i++ {
+						idx := (i * 2)
+						setBindingInEnv(newEnv, bindings.Value[idx:idx+2])
 					}
-					return setBindingInEnv(env, v.Value[1:])
-				case "let*":
-					newEnv := mal.NewEnv(env, nil, nil)
-					if len(v.Value) < 3 {
-						return nil, fmt.Errorf("'let*' expects at least 2 paramters")
-					}
-					if bindings, ok := v.Value[1].(*mal.List); ok {
-						for i := 0; i < len(bindings.Value)/2; i++ {
-							idx := (i * 2)
-							setBindingInEnv(newEnv, bindings.Value[idx:idx+2])
-						}
 
-						//TCO
-						env = newEnv
-						ast = v.Value[2]
-						continue
-					}
-					return nil, fmt.Errorf("'let!': invalid arguments")
-				case "do":
-					for _, val := range v.Value[1 : len(v.Value)-1] {
-						var err error
-						_, err = eval(val, env)
-						if err != nil {
-							return nil, err
-						}
-					}
-					//TCO
-					ast = v.Value[len(v.Value)-1]
-					continue
-				case "if":
-					r, err := eval(v.Value[1], env)
+					env = newEnv
+					ast = v.Value[2]
+					goto tailcalloptimized
+				}
+				return nil, fmt.Errorf("'let!': invalid arguments")
+			case "do":
+				for _, val := range v.Value[1 : len(v.Value)-1] {
+					var err error
+					_, err = eval(val, env)
 					if err != nil {
 						return nil, err
 					}
-					evaluatedTo := true
-					if b, ok := r.(*mal.Boolean); ok {
-						evaluatedTo = b.Value
-					}
-					if _, ok := r.(*mal.Nil); ok {
-						evaluatedTo = false
-					}
-					if evaluatedTo == true {
-						ast = v.Value[2] //TCO
-						continue         //TCO
-					}
-					//condition evaluated to false, check if we have a branch for false, and execute it, if so
-					if len(v.Value) < 4 {
-						return &mal.Nil{}, nil
-					}
-					ast = v.Value[3] //TCO
-					continue         //TCO
-				case "fn*":
-					bindings, ok := v.Value[1].(*mal.List)
-					if !ok {
-						return nil, fmt.Errorf("Invalid bindings to fn*")
-					}
-					return &mal.Function{
-						Ast:    v.Value[2],
-						Params: bindings.Value,
-						Env:    env,
-						Fn: func(args ...mal.Type) (mal.Type, error) {
-							fnEnv := mal.NewEnv(env, bindings.Value, args)
-							return eval(v.Value[2], fnEnv)
-						}}, nil
 				}
+				ast = v.Value[len(v.Value)-1]
+				goto tailcalloptimized
+			case "if":
+				r, err := eval(v.Value[1], env)
+				if err != nil {
+					return nil, err
+				}
+				evaluatedTo := true
+				if b, ok := r.(*mal.Boolean); ok {
+					evaluatedTo = b.Value
+				}
+				if _, ok := r.(*mal.Nil); ok {
+					evaluatedTo = false
+				}
+				if evaluatedTo == true {
+					ast = v.Value[2]
+					goto tailcalloptimized
+				}
+				//condition evaluated to false, check if we have a branch for false, and execute it, if so
+				if len(v.Value) < 4 {
+					return &mal.Nil{}, nil
+				}
+				ast = v.Value[3]
+				goto tailcalloptimized
+			case "fn*":
+				bindings, ok := v.Value[1].(*mal.List)
+				if !ok {
+					return nil, fmt.Errorf("Invalid bindings to fn*")
+				}
+				return &mal.Function{
+					Ast:    v.Value[2],
+					Params: bindings.Value,
+					Env:    env,
+					Fn: func(args ...mal.Type) (mal.Type, error) {
+						fnEnv := mal.NewEnv(env, bindings.Value, args)
+						return eval(v.Value[2], fnEnv)
+					}}, nil
 			}
-
-			ev, err := evalAst(v, env)
-			if err != nil {
-				return nil, err
-			}
-			lst, _ := ev.(*mal.List)
-			fn, _ := lst.Value[0].(*mal.Function)
-			//if we have an AST (and params/env), we can TCO this function!
-			if fn.Ast != nil {
-				ast = fn.Ast
-				//update the env for the function
-				env = mal.NewEnv(fn.Env, fn.Params, lst.Value[1:])
-				continue
-			}
-			//cannot TCO this (e.g. call to native function)
-			return fn.Fn(lst.Value[1:]...)
-
-		default:
-			return evalAst(v, env)
 		}
+
+		ev, err := evalAst(v, env)
+		if err != nil {
+			return nil, err
+		}
+		lst, _ := ev.(*mal.List)
+		fn, _ := lst.Value[0].(*mal.Function)
+		//if we have an AST (and params/env), we can TCO this function!
+		if fn.Ast != nil {
+			ast = fn.Ast
+			//update the env for the function
+			env = mal.NewEnv(fn.Env, fn.Params, lst.Value[1:])
+			goto tailcalloptimized
+		}
+		//cannot TCO this (e.g. call to native function)
+		return fn.Fn(lst.Value[1:]...)
+
+	default:
+		return evalAst(v, env)
 	}
+
 }
 
 func evalAst(ast mal.Type, env *mal.Env) (mal.Type, error) {
